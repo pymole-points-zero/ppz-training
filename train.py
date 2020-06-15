70e8de74d6bbec1273c9a83825e22b03ce45719f
pymole <goglbummm@gmail.com>
2020-06-16 02:36:53 +0500

    training server, chunks load

--- train.py
+++ train.py
@@ -7,21 +7,47 @@ import argparse
 import json
 import datetime
 import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 import gzip
 import io
 import tempfile
+import tarfile
 from tensorflow import keras
 from tensorflow.keras.optimizers import SGD
+import tensorflow as tf
 
 
-def upload(model_path, url, params):
+retry_strategy = Retry(
+    total=5,
+    status_forcelist=[429, 500, 502, 503, 504],
+    method_whitelist=["GET", "POST"],
+    backoff_factor=10
+)
+
+adapter = HTTPAdapter(max_retries=retry_strategy)
+
+
+def prepare_session():
+    s = requests.Session()
+    s.mount("https://", adapter)
+    s.mount("http://", adapter)
+
+    return s
+
+
+def upload(model_path, url, best_sha, training_run_id, params):
     with open(model_path, 'rb') as f:
         network = io.BytesIO(gzip.compress(f.read()))
 
     files = {'network': network}
+    params['prev_delta_sha'] = best_sha
+    params['training_run_id'] = training_run_id
+
+    session = prepare_session()
 
     try:
-        response = requests.post(url, data=params, files=files)
+        response = session.post(url, data=params, files=files)
         print(response.json())
     except Exception as e:
         print(e)
@@ -53,13 +79,17 @@ def load_model(tmp_dir, model_path):
 
 
 def get_positions(latest_chunks):
-    positions = []
-    for path in latest_chunks:
-        with gzip.open(path, 'rb') as f:
-            chunks = np.load(f, allow_pickle=True)
-            positions.append(chunks)
+    examples = []
+    for chunk_path in latest_chunks:
+        with tarfile.open(chunk_path, 'r:gz') as tar_chunk:
+            for game in tar_chunk:
+                gziped_examples = tar_chunk.extractfile(game)
+                example = gzip.decompress(gziped_examples.read())
+                byte_example = io.BytesIO(example)
+                example = np.load(byte_example, allow_pickle=True)
+                examples.append(example)
 
-    positions = np.concatenate(positions)
+    positions = np.concatenate(examples)
     np.random.shuffle(positions)
     print(len(positions), 'positions')
 
@@ -67,12 +97,12 @@ def get_positions(latest_chunks):
 
 
 def get_all_chunks(path):
-    return [os.path.join(path, filename) for filename in glob.glob(os.path.join(path, '*.gz'))]
+    return glob.glob(os.path.join(path, '*.tar.gz'))
 
 
 def get_latest_chunks(path, pool_size, num_chunks, allow_less):
     chunk_paths = get_all_chunks(path)
-    print(len(chunk_paths), 'games at all')
+    print('Found', len(chunk_paths), 'chunks at all')
 
     if len(chunk_paths) < pool_size:
         if not allow_less:
@@ -84,24 +114,80 @@ def get_latest_chunks(path, pool_size, num_chunks, allow_less):
     pool = chunk_paths[:pool_size]
     print(f'Constructed pool. {len(pool)} of {pool_size}')
 
-    print('First game generated at ', datetime.datetime.fromtimestamp(os.path.getmtime(pool[0])))
-    print('Last game generated at ', datetime.datetime.fromtimestamp(os.path.getmtime(pool[-1])))
+    print('First game generated at ', datetime.datetime.fromtimestamp(os.path.getmtime(pool[-1])))
+    print('Last game generated at ', datetime.datetime.fromtimestamp(os.path.getmtime(pool[0])))
 
-    # get random games from pool
+    # get random chunks from pool
     random.shuffle(chunk_paths)
     chunk_paths = pool[:num_chunks]
 
-    print(f"Randomly selected {chunk_paths} games from pool")
+    print(f"Randomly selected {len(chunk_paths)} chunks from pool")
 
     return chunk_paths
+
+
+def download_network(url, model_path, sha):
+    if os.path.exists(model_path):
+        print(sha, 'is cached')
+        return
+
+    print('Downloading network', sha)
+    session = prepare_session()
+
+    try:
+        response = session.get(url, json={'sha': sha})
+        with open(model_path, 'wb') as f:
+            f.write(response.content)
+    except:
+        raise Exception
+
+
+def download_chunks(list_url, download_url, input_path):
+    print('Downloading chunks')
+    # get latest chunks from server
+    session = prepare_session()
+
+    response = session.get(list_url)
+    server_latest_chunks = response.json()
+    # get missing files
+    all_local_chunks = map(os.path.basename, get_all_chunks(input_path))
+    missing_chunks = list(set(server_latest_chunks) - set(all_local_chunks))
+    print(f'Missing {len(missing_chunks)} chunks')
+
+    # download missing files
+    if missing_chunks:
+        response = requests.get(download_url, json=missing_chunks)
+        missing_chunks_file = io.BytesIO(response.content)
+
+        with tarfile.open(fileobj=missing_chunks_file, mode='r:gz') as tar:
+            tar.extractall(input_path)
 
 
 def main(args):
     with open(args.config, 'r') as f:
         config = json.load(f)
 
+    server_conf = config.get('server', None)
+
+    if server_conf is not None:
+        if 'examples' in server_conf:
+            config['input_path'] = os.path.join('examples', str(server_conf['training_run_id']))
+            os.makedirs(config['input_path'], exist_ok=True)
+
+            download_chunks(server_conf['examples']['list_url'], server_conf['examples']['download_url'],
+                            config['input_path'])
+
+        if 'network' in server_conf:
+            sha = server_conf['best_sha']
+            config['model_input'] = os.path.join('networks', sha+'.gz')
+            if not os.path.exists('networks'):
+                os.mkdir('networks')
+
+            download_network(server_conf['network']['url'], config['model_input'], sha)
+
     latest_chunks = get_latest_chunks(config['input_path'], config['pool_size'],
                                       config['num_chunks'], config['allow_less'])
+
     positions = get_positions(latest_chunks)
     # train, test = train_test_split(examples, config.train_ratio)
 
@@ -117,21 +203,31 @@ def main(args):
         model.save(config['model_output'], save_format='h5', include_optimizer=False)
 
     # TODO separate upload and train into different files
-    if 'upload' in config:
+    if server_conf is not None and 'upload' in server_conf:
         if 'model_output' in config:
-            upload(config['model_output'], config['upload']['url'], config['upload']['params'])
+            upload(config['model_output'], server_conf['upload']['url'],
+                   server_conf['best_sha'], server_conf['training_run_id'],
+                   server_conf['upload']['params'])
         else:
             temp_model_path = os.path.join(os.getcwd(), 'temp_model.h5')
             model.save(temp_model_path, save_format='h5', include_optimizer=False)
 
-            upload(temp_model_path, config['upload']['url'], config['upload']['params'])
+            upload(temp_model_path, server_conf['upload']['url'],
+                   server_conf['best_sha'], server_conf['training_run_id'],
+                   server_conf['upload']['params'])
 
             os.remove(temp_model_path)
 
 
+# TODO logger
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # TODO inside program it should be config_path
     parser.add_argument('--config', type=str)
+
+    gpus = tf.config.list_physical_devices('GPU')
+
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
 
     main(parser.parse_args())
---
+++ training_server.py
@@ -0,0 +1,55 @@
+import argparse
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import subprocess
+import json
+import os
+
+CWD = os.getcwd()
+
+
+class TrainingServer(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.send_response(200)
+        self.end_headers()
+
+        content_length = int(self.headers['Content-Length'])
+        # read json config
+        config = self.rfile.read(content_length)
+        print('Starting training with config: ', config)
+        config = json.loads(config)
+
+        # create config file
+        with open('configs/config.json', 'w') as f:
+            json.dump(config, f)
+
+        # start training process
+        args = [
+            os.path.join(CWD, 'venv', 'bin', 'python'),
+            os.path.join(CWD, 'train.py'),
+            '--config', os.path.join(CWD, 'configs/config.json')
+        ]
+        subprocess.Popen(' '.join(args), shell=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run training server")
+    parser.add_argument(
+        "-l",
+        "--listen",
+        default="localhost",
+        help="Specify the IP address on which the server listens",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8000,
+        help="Specify the port on which the server listens",
+    )
+    args = parser.parse_args()
+
+    server_address = (args.listen, args.port)
+    training_server = HTTPServer(server_address, TrainingServer)
+
+    print(f"Starting training server on {args.listen}:{args.port}")
+    training_server.serve_forever()
